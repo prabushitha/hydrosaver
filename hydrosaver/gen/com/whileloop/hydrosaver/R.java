d95d86924e19f220ae45df012bea4a409d6dbbe4
uprabushitha <prabushitha@gmail.com>
2016-10-08 19:10:21 +0530

    android project added

--- hydrosaver/gen/com/whileloop/hydrosaver/R.java
+++ hydrosaver/gen/com/whileloop/hydrosaver/R.java
@@ -10,6 +10,10 @@ package com.whileloop.hydrosaver;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int titlebackgroundcolor=0x7f040000;
+        public static final int titletextcolor=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,42 +22,64 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int navigation_drawer_width=0x7f040002;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int navigation_drawer_width=0x7f050002;
     }
     public static final class drawable {
         public static final int drawer_shadow=0x7f020000;
-        public static final int ic_drawer=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int graph1=0x7f020001;
+        public static final int ic_drawer=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int logo=0x7f020004;
+        public static final int mybutton=0x7f020005;
+        public static final int section_button=0x7f020006;
+        public static final int section_tabs=0x7f020007;
+        public static final int syncbutton=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int btnLogin=0x7f080003;
-        public static final int btnLogout=0x7f080000;
-        public static final int editPass=0x7f080002;
-        public static final int editUser=0x7f080001;
+        public static final int action_settings=0x7f090011;
+        public static final int btnLogin=0x7f09000d;
+        public static final int btnLogout=0x7f09000a;
+        public static final int btncommunity=0x7f090009;
+        public static final int btnsections=0x7f090007;
+        public static final int btnsync=0x7f090005;
+        public static final int editPass=0x7f09000c;
+        public static final int editUser=0x7f09000b;
+        public static final int graphMainPie=0x7f090002;
+        public static final int lifetime=0x7f09000f;
+        public static final int mainUnitsText=0x7f090003;
+        public static final int scrollView1=0x7f090000;
+        public static final int section1_label=0x7f090010;
+        public static final int tableRow2=0x7f090001;
+        public static final int tableRow4=0x7f090006;
+        public static final int tableRow5=0x7f090008;
+        public static final int tableRow6=0x7f090004;
+        public static final int thismonth=0x7f09000e;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_sections=0x7f030002;
     }
     public static final class menu {
-        public static final int global=0x7f070000;
-        public static final int home=0x7f070001;
-        public static final int main=0x7f070002;
+        public static final int global=0x7f080000;
+        public static final int home=0x7f080001;
+        public static final int main=0x7f080002;
+        public static final int sections=0x7f080003;
     }
     public static final class string {
-        public static final int action_example=0x7f050009;
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int navigation_drawer_close=0x7f050008;
-        public static final int navigation_drawer_open=0x7f050007;
-        public static final int title_activity_home=0x7f050003;
-        public static final int title_section1=0x7f050004;
-        public static final int title_section2=0x7f050005;
-        public static final int title_section3=0x7f050006;
+        public static final int action_example=0x7f060009;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int navigation_drawer_close=0x7f060008;
+        public static final int navigation_drawer_open=0x7f060007;
+        public static final int title_activity_home=0x7f060003;
+        public static final int title_activity_sections=0x7f06000a;
+        public static final int title_section1=0x7f060004;
+        public static final int title_section2=0x7f060005;
+        public static final int title_section3=0x7f060006;
     }
     public static final class style {
         /** 
@@ -77,10 +103,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
---
+++ hydrosaver/src/com/whileloop/hydrosaver/Device.java
@@ -0,0 +1,18 @@
+package com.whileloop.hydrosaver;
+
+public class Device {
+	public float units; //
+	public float volume;
+	public String period;
+	
+	
+	
+	
+	
+	public float calculateCost(){
+		//cost making algorithm here
+		return units*3;
+	}
+	
+	
+}
